ed536a159f91f0b9cca28122d67ce0e3bf798903
Duke <dukekhaos@gmail.com>
2015-09-20 17:41:48 -0300

    fix grid size, and show Grid config

--- src/grid.js
+++ src/grid.js
@@ -6,7 +6,23 @@ var Grid = React.createClass({
     return { width: this.props.initialWidth, id: 0, size: 1 };
   },
   render() {
-    return <div>Hello {this.props.name}</div>;
+    const width = this.props.initialWidth * this.state.size;
+    const style = {
+      width:  width,
+      height: width * 0.7
+    };
+    return <div style={style} className="grid">
+      <div>
+        <pre><code>
+        {'{'}{'\n'}
+          id: {this.state.id},{'\n'}
+          initialWidth: {this.props.initialWidth},{'\n'}
+          width: {width}px,{'\n'}
+          size: {this.state.size}{'\n'}
+        {'}'}
+      </code></pre>
+      </div>
+    </div>;
   }
 })
 
