682b9c6038a5e274489f9208e867e37acbfcdf47
Duke <dukekhaos@gmail.com>
2015-09-20 17:44:29 -0300

    add 'add grid' button

--- src/layout.js
+++ src/layout.js
@@ -4,8 +4,13 @@ var Layout = React.createClass({
   getInitialState() {
     return { grids: [], containerSize: 1000, columns: 4 };
   },
+  handleAdd() {
+    var grids = this.state.grids.concat([ {} ]);
+    this.setState({grids: grids});
+  },
   render() {
     return <div>
+      <button onClick={this.handleAdd}>Add Grid</button>
       <div ref="container" style={style} id="container">{
         this.state.grids.map((grid) => {
           return <Grid columns={ this.state.columns } initialWidth={ this.state.containerSize / this.state.columns } />
