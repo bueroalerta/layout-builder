5119a382c2a3515c8db7f886f638812a8f4158fa
Duke <dukekhaos@gmail.com>
2015-09-20 17:40:41 -0300

    add width, id and size to Grid

--- src/grid.js
+++ src/grid.js
@@ -1,4 +1,10 @@
 var Grid = React.createClass({
+  componentDidMount() {
+    this.setState({ id: pckry.items.length } );
+  },
+  getInitialState() {
+    return { width: this.props.initialWidth, id: 0, size: 1 };
+  },
   render() {
     return <div>Hello {this.props.name}</div>;
   }
