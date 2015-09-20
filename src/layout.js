a9a70921904d965988dc62e092df63ccc46fb079
Duke <dukekhaos@gmail.com>
2015-09-20 17:45:04 -0300

    add input to config container size and columns number

--- src/layout.js
+++ src/layout.js
@@ -8,8 +8,25 @@ var Layout = React.createClass({
     var grids = this.state.grids.concat([ {} ]);
     this.setState({grids: grids});
   },
+  setContainerSize(event) {
+    this.setState({ containerSize: event.target.value });
+    setTimeout(() => { pckry.layout() }, 2);
+  },
+  setColumns(event) {
+    this.setState({ columns: event.target.value });
+    setTimeout(() => { pckry.layout() }, 2);
+  },
   render() {
+    const style = { width: this.state.containerSize }
     return <div>
+      <label>
+        Container Size:
+        <input value={this.state.containerSize} onChange={this.setContainerSize} />
+      </label>
+      <label>
+        Columns:
+        <input value={this.state.columns} onChange={this.setColumns} />
+      </label>
       <button onClick={this.handleAdd}>Add Grid</button>
       <div ref="container" style={style} id="container">{
         this.state.grids.map((grid) => {
